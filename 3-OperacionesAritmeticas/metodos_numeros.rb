a86450afe94c8b98bdbea2a38dc21a625f835cba
developerharo <developerharo@gmail.com>
2021-09-23 23:01:57 -0500

    3-OperacionesAritmeticas

---
+++ 3-OperacionesAritmeticas/metodos_numeros.rb
@@ -0,0 +1,21 @@
+#Metodos de números, en Ruby los numeros
+#son objetos
+
+#Dar el siguiente de un numero
+puts 8.next
+
+#Dar el anterior de un numero
+puts 8.pred
+
+#Comprobar si el numero es cero 
+puts 8.zero?
+
+#Comprobar si es par
+puts 8.even?
+
+#Comprobar si es non
+puts 8.odd?
+
+#Comprobar el tipo de clase
+puts 8888.class
+
